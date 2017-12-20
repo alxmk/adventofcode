01bdecb7a4926d23fb9f1654f20a387afd474e3c
Alexander Mckenzie-Kelly <alexmk@uk.ibm.com>
2017-12-20 14:54:03 +0000

    Solved day 9

---
+++ day9/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+	"text/scanner"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalf("Error reading input file %v", err)
+	}
+
+	scan := &scanner.Scanner{}
+
+	scan.Init(strings.NewReader(string(input)))
+	// Read off the first {
+	if scan.Scan() != '{' {
+		log.Fatalf("Error - input didn't start with {")
+	}
+
+	group := parse(scan, 1)
+
+	log.Println("The answer is", group.Score())
+}
+
+func parse(scan *scanner.Scanner, score int) *group {
+	group := &group{
+		score: score,
+	}
+
+	garbage := false
+
+	for {
+		c := scan.Next()
+
+		group.me += string(c)
+
+		switch {
+		case c == '<' && !garbage:
+			garbage = true
+		case c == '>':
+			garbage = false
+		case c == '{' && !garbage:
+			group.children = append(group.children, parse(scan, score+1))
+		case c == '}' && !garbage:
+			return group
+		case c == '!':
+			// Toss the next one and continue
+			scan.Next()
+		case c == scanner.EOF:
+			return group
+		default:
+			// Just continue
+		}
+	}
+}
+
+type group struct {
+	children []*group
+	score    int
+	me       string
+}
+
+func (g *group) Score() int {
+	total := g.score
+
+	for _, c := range g.children {
+		total += c.Score()
+	}
+
+	return total
+}
+
+func (g *group) Print() string {
+	out := fmt.Sprintf("%s I have score %d and %d children.\n", g.me, g.score, len(g.children))
+
+	for _, c := range g.children {
+		out += c.Print()
+	}
+
+	return out
+}
