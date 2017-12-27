e2a0fa08907b291cb9298e3bdc2e01454eb23580
Alexander Mckenzie-Kelly <alexmk@uk.ibm.com>
2017-12-27 13:29:44 +0000

    Solved day 4 part 2

--- day4/main.go
+++ day4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"sort"
 	"strings"
 )
 
@@ -30,5 +31,55 @@ outer:
 		numValid++
 	}
 
-	log.Println("The answer is", numValid)
+	log.Println("Part 1 answer is", numValid)
+
+	numValid = 0
+
+invalid:
+	for _, passphrase := range passphrases {
+		sizes := make(map[int][]string)
+		for _, word := range strings.Fields(passphrase) {
+			sizes[len(word)] = append(sizes[len(word)], word)
+		}
+
+		for size, words := range sizes {
+			if size < 2 {
+				continue
+			}
+
+			found := map[string]struct{}{}
+
+			for _, word := range words {
+				ordered := SortString(word)
+				if _, ok := found[ordered]; ok {
+					continue invalid
+				}
+				found[ordered] = struct{}{}
+			}
+		}
+
+		numValid++
+	}
+
+	log.Println("Part 2 answer is", numValid)
+}
+
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
 }
