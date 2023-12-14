2cfbbf24e9f0400bcef7c3459cf877f2faaad4c4
Alexander Mckenzie-Kelly <alexmk@uk.ibm.com>
2023-12-14 09:10:59 +0000

    Refactor 2023 day 14

--- 2023/day14/main.go
+++ 2023/day14/main.go
@@ -16,7 +16,7 @@ func main() {
 	log.Println("Part two:", partTwo(parsePlatform(string(data)), 1000000000))
 }
 
-func partTwo(p platform, spins int) int {
+func partTwo(p *platform, spins int) int {
 	cache := make(map[string]int)
 	var repeat bool
 	for i := 0; i < spins; i++ {
@@ -31,8 +31,8 @@ func partTwo(p platform, spins int) int {
 	return p.Load()
 }
 
-func parsePlatform(input string) platform {
-	p := platform{rocks: make(map[xy]rune)}
+func parsePlatform(input string) *platform {
+	p := &platform{rocks: make(map[xy]rune)}
 	for y, line := range strings.Split(input, "\n") {
 		p.max.y = y
 		for x, r := range line {
@@ -72,84 +72,42 @@ func (p platform) String() string {
 	return b.String()
 }
 
-func (p platform) Spin() platform {
+func (p *platform) Spin() *platform {
 	for _, d := range []xy{n, w, s, e} {
-		p = p.Tilt(d)
+		p.Tilt(d)
 	}
 	return p
 }
 
-func (p platform) Tilt(dir xy) platform {
+func (p *platform) Tilt(dir xy) *platform {
 	switch dir {
-	case n:
-		for y := 0; y <= p.max.y; y++ {
-			for x := 0; x <= p.max.x; x++ {
-				if p.rocks[xy{x, y}] != 'O' {
-					continue
-				}
-				p.rocks[xy{x, y}] = '.'
-				for j := y; j >= -1; j-- {
-					if p.rocks[xy{x, j}] == '.' {
-						continue
-					}
-					p.rocks[xy{x, j + 1}] = 'O'
-					break
-				}
-			}
-		}
-	case s:
-		for y := p.max.y; y >= 0; y-- {
-			for x := p.max.x; x >= 0; x-- {
-				if p.rocks[xy{x, y}] != 'O' {
-					continue
-				}
-				p.rocks[xy{x, y}] = '.'
-				for j := y; j <= p.max.y+1; j++ {
-					if p.rocks[xy{x, j}] == '.' {
-						continue
-					}
-					p.rocks[xy{x, j - 1}] = 'O'
-					break
-				}
-			}
-		}
-	case e:
-		for x := p.max.x; x >= 0; x-- {
-			for y := p.max.y; y >= 0; y-- {
-				if p.rocks[xy{x, y}] != 'O' {
-					continue
-				}
-				p.rocks[xy{x, y}] = '.'
-				for j := x; j <= p.max.x+1; j++ {
-					if p.rocks[xy{j, y}] == '.' {
-						continue
-					}
-					p.rocks[xy{j - 1, y}] = 'O'
-					break
-				}
-			}
-		}
-	case w:
-		for x := 0; x <= p.max.x; x++ {
-			for y := 0; y <= p.max.y; y++ {
-				if p.rocks[xy{x, y}] != 'O' {
-					continue
-				}
-				p.rocks[xy{x, y}] = '.'
-				for j := x; j >= -1; j-- {
-					if p.rocks[xy{j, y}] == '.' {
-						continue
-					}
-					p.rocks[xy{j + 1, y}] = 'O'
-					break
-				}
-			}
-		}
+	case n, w:
+		p.tiltInner(xy{0, 0}, xy{p.max.x + 1, p.max.y + 1}, xy{1, 1}, dir)
+	case e, s:
+		p.tiltInner(xy{p.max.x, p.max.y}, xy{-1, -1}, xy{-1, -1}, dir)
 	}
 	return p
 }
 
-func (p platform) Load() int {
+func (p *platform) tiltInner(lower, upper, inc, dir xy) {
+	for x := lower.x; x != upper.x; x += inc.x {
+		for y := lower.y; y != upper.y; y += inc.y {
+			if p.rocks[xy{x, y}] != 'O' {
+				continue
+			}
+			p.rocks[xy{x, y}] = '.'
+			for j := (xy{x, y}); ; j = (xy{j.x + dir.x, j.y + dir.y}) {
+				if p.rocks[j] == '.' {
+					continue
+				}
+				p.rocks[xy{j.x - dir.x, j.y - dir.y}] = 'O'
+				break
+			}
+		}
+	}
+}
+
+func (p *platform) Load() int {
 	var load int
 	for pos, r := range p.rocks {
 		if r != 'O' {
